ad751688b0979c5567449ee472a7ce354d936d36
sophiebr <n/a>
2017-08-17 08:52:31 -0500

    finalized changes and added screenshot for README

--- flexappjs.js
+++ flexappjs.js
@@ -4,7 +4,6 @@ var path = require('path')
 const serveStatic = require('serve-static');
 
 myapp.use('/', serveStatic("public"))
-// myapp.use(express.static("public"))
 
 myapp.listen(3000, function(){
     console.log('Listening')
